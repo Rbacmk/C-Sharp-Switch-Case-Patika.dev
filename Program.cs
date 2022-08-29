a278d6ec9fb18571137ad622126ef5b20d899fd7
Rabia Çakmak <ozancakmak867@gmail.com>
2022-08-29 14:16:46 +0300

    Proje dosyası ekle.

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Switch_Case
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+# Switch-Case
+            int month = DateTime.Now.Month;
+            switch (month)// Expression -Kontrol etmek isteğimiz koşulu
+            {
+                case 1:
+                    Console.WriteLine("Ocak ayındasınız:");
+                    break;
+                case 2:
+                    Console.WriteLine("Şubat ayındasınız:");
+                    break;
+                case 3:
+                    Console.WriteLine("Mart ayındasınız:");
+                    break;
+                case 4:
+                    Console.WriteLine("Nisan ayındasınız:");
+                    break;
+
+                default:
+                    Console.WriteLine("Yanlış veri girdiniz:");
+                    break;
+            }
+            switch (month)
+            {
+                case 12:
+                case 1:
+                case 3:
+                    Console.WriteLine("Kış ayındasınız.");
+                    break;
+
+
+                default;
+                    break;
+            }
+        }
+    }
+
+}
+    }
+}
